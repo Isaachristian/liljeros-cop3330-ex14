54e01dd7b4fd57c424892d380ae5b0923a04b258
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-06-01 20:14:08 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Tax Calculator
+ */
+public class App {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Variables
+        double amount;
+        String state;
+
+        System.out.print("What is the order amount? ");
+        amount = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("What is the State? ");
+        state = scanner.nextLine();
+
+        if (state.equals("WI")) {
+            double tax = amount * 5.5 / 100;
+            System.out.printf("The subtotal is $%.2f%n", amount);
+            System.out.printf("The tax is $%.2f%n", tax);
+            amount += tax;
+        }
+
+        System.out.printf("The total is $%.2f", amount);
+    }
+}
